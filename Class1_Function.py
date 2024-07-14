5c398d2ad95e3f9c109b7401abd3c947c0e82506
Marina Cantarelli <macbarroca@gmail.com>
2024-07-14 13:13:00 -0300

    applying a module

--- Class1_Function.py
+++ Class1_Function.py
@@ -6,4 +6,8 @@ def add():
 def mult():
     print('This function will multiplicate values.')
 
-
+def find_index(to_find, item):
+    for i, value in enumerate(to_find):
+        if value == item:
+            return 1 
+    return -1 
---
+++ Class4_Main.py
@@ -0,0 +1,7 @@
+#Applying a module
+from Class1_Function import find_index
+
+list1 = ['a', 'b', 'c', 'd', 'e', 'f']
+
+var1 = find_index(list1, 'f')
+print(var1)
